806a4d1862090be010487eb050f5d3f8f868e247
Ann <ann.hixson@uconn.edu>
2023-01-04 16:09:25 -0500

    fixed profile bugs

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -2,30 +2,39 @@ const router = require('express').Router();
 const { User, BlogPost, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
   try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: BlogPost }],
+    });
+
+    const user = userData.get({ plain: true });
+
     const blogPostData = await BlogPost.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
       include: [
         {
           model: User,
           attributes: ['name'],
         }
-      ],
+      ]
+
     });
 
     const blogPosts = blogPostData.map((blogPost) => blogPost.get({ plain: true }));
-
+    
     res.render('profile', { 
-      blogPosts, 
+      blogPosts,
+      user,
       logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
-
-
 
 // router.get('/blogposts/:id', async (req, res) => {
 //   try {
@@ -58,23 +67,5 @@ router.get('/', async (req, res) => {
 //     res.status(500).json(err);
 //   }
 // });
-
-router.get('/', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: BlogPost }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 module.exports = router;
